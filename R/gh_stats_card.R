8d00f388ff88f92456172049b1e80b71aadc90dd
Kittipos <ki11ip0.s.a.s@gmail.com>
2021-12-29 22:39:49 +0700

    Add *url version, `theme` args no longer pre-checked

--- R/gh_stats_card.R
+++ R/gh_stats_card.R
@@ -2,14 +2,13 @@
 
 # Stats Card --------------------------------------------------------------
 
-
 #' Create GitHub Stats Card 
 #'
 #' @param username Your Github username
 #' @param hide If not `NULL`, provide stats to hide, one of: stars, commits, prs, issues, contribs.
 #' @param count_private (Boolean) `TRUE` to add the count of all your private contributions to the total commits count
 #' @param show_icons (Boolean) `TRUE` to enable icons
-#' @param theme If not `NULL`, provide build in theme names, one of: dark, radical, merko, gruvbox, tokyonight, onedark, cobalt, synthwave, highcontrast, dracula.
+#' @param theme If not `NULL`, provide build in theme names, e.g., one of: dark, radical, merko, gruvbox, tokyonight, onedark, cobalt, synthwave, highcontrast, dracula.
 #'
 #' @return A character rendering GitHub Stats Card to be inserted into R Markdown "github_document"
 #' @export
@@ -21,6 +20,41 @@ gh_stats_card <- function(username,
                           show_icons = FALSE,
                           theme = NULL
 ){
+  
+  url <- gh_stats_card_url(username = username, 
+                           hide = hide, 
+                           count_private = count_private, 
+                           show_icons = show_icons, 
+                           theme = theme)
+  
+  ### Build Markdown Link
+  glue::glue("[![{username} GitHub stats]({url})](https://github.com/{username}/{username})")
+  
+}
+
+
+# Github Stats Card URL ---------------------------------------------------
+
+
+
+#' Create GitHub Stats Card URL 
+#'
+#' @param username Your Github username
+#' @param hide If not `NULL`, provide stats to hide, one of: stars, commits, prs, issues, contribs.
+#' @param count_private (Boolean) `TRUE` to add the count of all your private contributions to the total commits count
+#' @param show_icons (Boolean) `TRUE` to enable icons
+#' @param theme If not `NULL`, provide build in theme names, e.g., one of: dark, radical, merko, gruvbox, tokyonight, onedark, cobalt, synthwave, highcontrast, dracula.
+#'
+#' @return A URL of GitHub Stats Card
+#' @export
+#' @source https://github.com/anuraghazra/github-readme-stats
+#' 
+gh_stats_card_url <- function(username,
+                              hide = NULL,
+                              count_private = FALSE,
+                              show_icons = FALSE,
+                              theme = NULL
+) {
   
   ## Hide Individual Stats
   hide <- if (is.null(hide)) {
@@ -40,7 +74,7 @@ gh_stats_card <- function(username,
     ""
   }
   
-  ## Show Icons 
+  ## Show Icons
   show_icons <- if (show_icons) {
     "&show_icons=true"
   } else {
@@ -50,19 +84,10 @@ gh_stats_card <- function(username,
   theme <- if (is.null(theme)) {
     ""
   } else {
-    ## Choose Theme
-    theme_nm <- match.arg(theme,
-                          choices = c("dark", "radical", "merko", "gruvbox", 
-                                      "tokyonight", "onedark", "cobalt", "synthwave", 
-                                      "highcontrast", "dracula")
-    )
-    paste0("&theme=", theme_nm)
+    paste0("&theme=", theme)
   }
   
   ### Build Markdown Link
-  glue::glue("[![{username} GitHub stats](https://github-readme-stats.vercel.app/api?username={username}{hide}{count_private}{show_icons}{theme})](https://github.com/{username}/github-readme-stats)")
-  
+  glue::glue("https://github-readme-stats.vercel.app/api?username={username}{hide}{count_private}{show_icons}{theme}")
 }
-
-
 
--- R/gh_top_lang_card.R
+++ R/gh_top_lang_card.R
@@ -1,4 +1,5 @@
 
+# Top Language Card ---------------------------------------------------
 
 #' Create Github Top Languages Card
 #'
@@ -7,16 +8,55 @@
 #' @param hide if not `NULL`, specify character vector of language name(s) to hide
 #' @param langs_count Option to specify the number of languages shown on the card, must be an integer from 1 to 10, default is 5.
 #' @param layout_compact if `TRUE`, card will show in a compact layout
+#' @param theme If not `NULL`, provide build in theme names
 #'
 #' @return A character rendering GitHub Top Languages Card to be inserted into R Markdown "github_document"
 #' @export
 #' @source https://github.com/anuraghazra/github-readme-stats
-#'
+#' 
 gh_top_lang_card <- function(username,
                              exclude_repo = NULL,
                              hide = NULL,
                              langs_count = 5,
-                             layout_compact = FALSE
+                             layout_compact = FALSE,
+                             theme = NULL
+){
+  
+  url <- gh_top_lang_card_url(username = username,
+                              exclude_repo = exclude_repo, 
+                              hide = hide, 
+                              langs_count = langs_count, 
+                              layout_compact = layout_compact)
+  
+  # Build URL
+  glue::glue("[![Top Langs]({url})](https://github.com/{username}/{username})
+")
+  
+}
+
+
+
+# Top Language Card URL ---------------------------------------------------
+
+#' Create Github Top Languages Card URL
+#'
+#' @param username Your Github username
+#' @param exclude_repo if not `NULL`, specify character vector of repo name(s) to exclude individual repositories.
+#' @param hide if not `NULL`, specify character vector of language name(s) to hide
+#' @param langs_count Option to specify the number of languages shown on the card, must be an integer from 1 to 10, default is 5.
+#' @param layout_compact if `TRUE`, card will show in a compact layout
+#' @param theme If not `NULL`, provide build in theme names
+#'
+#' @return A URL of GitHub Top Languages Card 
+#' @export
+#' @source https://github.com/anuraghazra/github-readme-stats
+#' 
+gh_top_lang_card_url <- function(username,
+                                 exclude_repo = NULL,
+                                 hide = NULL,
+                                 langs_count = 5,
+                                 layout_compact = FALSE,
+                                 theme = NULL
 ){
   
   ## Exclude Individual Repo
@@ -48,8 +88,16 @@ gh_top_lang_card <- function(username,
     ""
   }
   
+  ## Theme
+  theme <- if (is.null(theme)) {
+    ""
+  } else {
+    ## Choose Theme
+    paste0("&theme=", theme)
+  }
+  
+  
   # Build URL
-  glue::glue("[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username={username}{exclude_repo}{hide}{langs_count}{layout_compact})](https://github.com/{username}/github-readme-stats)
-")
+  glue::glue("https://github-readme-stats.vercel.app/api/top-langs/?username={username}{exclude_repo}{hide}{langs_count}{layout_compact}{theme}")
   
 }
